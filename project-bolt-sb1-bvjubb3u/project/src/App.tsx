f440ae4f97667700136925c174f51f72a0196bac
Galeano2593 <juangaleano1126138@correo.itm.edu.co>
2026-07-21 20:26:37 -0500

    Clean up audio player comments and URL
    Removed comments about MP3 player setup and updated audio URL.

--- project-bolt-sb1-bvjubb3u/project/src/App.tsx
+++ project-bolt-sb1-bvjubb3u/project/src/App.tsx
@@ -4,8 +4,6 @@ import StartScreen from "./components/StartScreen";
 import Invitation from "./components/Invitation";
 import MusicPlayer from "./components/MusicPlayer";
 
-// Reproductor preparado para MP3. Agregar el archivo "Chiquitita - ABBA"
-// en /public/chiquitita.mp3 y dejar esta ruta, o reemplazar por la URL final.
 const AUDIO_URL = "/chiquitita.mp3";
 
 export default function App() {
